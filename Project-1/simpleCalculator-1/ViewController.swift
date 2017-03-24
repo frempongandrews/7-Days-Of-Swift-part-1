6d8034df378b48401e6ba53eafb011eab87f0d7c
Andrews Frempong <frempongandrews@yahoo.com>
2017-03-24 10:08:30 +0000

    first commit

---
+++ Project-1/simpleCalculator-1/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  simpleCalculator-1
+//
+//  Created by Andrews Frempong on 22/03/2017.
+//  Copyright © 2017 Andrews Frempong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func getNumber(_ sender: UIButton) {
+        //numbers pressed
+        //identify using tags
+        
+        
+    }
+    
+    @IBAction func getOperation(_ sender: UIButton) {
+        
+        //operation signs pressed
+        //identify using tags
+        
+    }
+    
+    
+    @IBAction func getResult(_ sender: UIButton) {
+        //equal sign pressed
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+
+
+}
+
