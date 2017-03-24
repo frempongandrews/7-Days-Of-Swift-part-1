8143987061da33928b4bef561cb64431744165fe
Andrews Frempong <frempongandrews@yahoo.com>
2017-03-24 16:58:30 +0000

    implementing addition

--- Project-1/simpleCalculator-1/ViewController.swift
+++ Project-1/simpleCalculator-1/ViewController.swift
@@ -9,21 +9,61 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var prevValue: Double = 0
+    var currentValue: Double = 0;
+    var isCalculating = false
+    var havePrevValue = false
+    
+    
+    @IBOutlet weak var screenLabel: UILabel!
+    
     @IBAction func getNumber(_ sender: UIButton) {
         //numbers pressed
         //identify using tags
         
-        print(sender.tag)
+//       print(sender.tag)
         
+        if (isCalculating) {
+            screenLabel.text = ""
+        }
         
+        if let screenValue = screenLabel.text {
+            screenLabel.text = screenValue + String(sender.tag)
+        }
+        
+        isCalculating = false
+        
+
     }
     
     @IBAction func getOperation(_ sender: UIButton) {
-        
+        //if I have prevValue, do not add any more numbers
+        if(havePrevValue) {
+            return
+        }
         //operation signs pressed
         //identify using tags
+        if (screenLabel.text != "" && screenLabel.text != "+" && sender.tag != 15 && sender.tag != 14) {
+            
+            prevValue = Double(screenLabel.text!)!
+            
+            //print(prevValue)
+            
+            if (sender.tag == 10) {
+                screenLabel.text = "+"
+                
+            }
+            
+            isCalculating = true
+            havePrevValue = true
+            
+        }
         
+        
+        //Equals
+       
+        
+    
     }
     
     
