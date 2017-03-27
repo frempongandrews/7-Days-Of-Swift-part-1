e1f76b62dec7b22e6df956c030f6103e84439a87
Andrews Frempong <frempongandrews@yahoo.com>
2017-03-27 15:24:19 +0100

    image showing on tap

---
+++ Project-2-imageViewer/Project-2-imageViewer/DetailViewController.swift
@@ -0,0 +1,49 @@
+//
+//  DetailViewController.swift
+//  Project-2-imageViewer
+//
+//  Created by Andrews Frempong on 27/03/2017.
+//  Copyright © 2017 Andrews Frempong. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    //selectedImage is optional because it won't exist the first time DetailViewController is loaded
+    var selectedImage: String?
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        //4- when it is loaded, show image
+            //To show image we need:
+            //a) image property on imageView and set it equal to a function
+            //b) The function is called UIImage which takes a named parameter and
+            //searches for that name in the app bundle to load it
+        
+        if let imageToLoad = self.selectedImage {
+            
+            imageView.image = UIImage(named: imageToLoad)
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Readme.md
+++ Readme.md
@@ -5,4 +5,4 @@ Hi Everyone
 
 I am Andrews Frempong, a frontend developer and currently learning Swift.
 
-This project is inspired by Sam Lu's 100 Days of Swift project. While learning Swift, I come to know about his project, and I made the decision to commit everyday to write Swift and learn about iOS development; this is the final code of the Swift projects.
+This project is inspired by Sam Lu's 100 Days of Swift project. While learning Swift, I came to know about his project, and I made the decision to commit everyday to write Swift and learn about iOS development; this is the final code of the projects I built.
